fe5e47ffe392fdb74754ee7a1c96ced7b05ee445
Will <billywells99@hotmail.com>
2022-10-11 00:59:51 +1100

    Account Repository

---
+++ src/main/java/com/example/s3718003_challenge2/repositories/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.example.s3718003_challenge2.repositories;
+
+import com.example.s3718003_challenge2.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account,Integer>
+{
+
+}
